91156bd7d194eaeb650bcd49c71b8d4ba985daec
Prashant-dot1 <puriprashant14@gmail.com>
2025-02-11 00:05:29 +0530

    testing the read logic from the log file path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+use ingest::{file_source::FileLogSource, LogSource};
+
 pub mod ingest;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut file_source = FileLogSource::new("./example.log");
+    file_source.init().await?;
+
+    while let Some(log_line) = file_source.read_line().await? {
+        println!("[{}] {}: {}",
+            log_line.timestamp,
+            log_line.source,
+            log_line.content
+        );
+    }
+
+    file_source.close().await?;
+    
+    Ok(())
 }
