e576696730a4e7bd8181b3e846f139c5cc161548
Prashant-dot1 <puriprashant14@gmail.com>
2025-02-14 01:58:39 +0530

    handling multi type parsing logic

--- src/ingest/network_source.rs
+++ src/ingest/network_source.rs
@@ -19,8 +19,54 @@ impl NetworkLogSource {
         }
     }
 
-    fn is_valid_json(content : &String) -> bool {
-        serde_json::from_str::<serde_json::Value>(content).is_ok()
+    fn try_extracting_json(content : &String) -> Option<(String , String)> {
+        
+        let mut depth = 0;
+        let mut within_json_string = false;
+        let mut escape_char_next = false;
+        let mut start_idx = None;
+
+
+        // finding the fist character
+        for (i, c) in content.chars().enumerate() {
+            if c == '{' && !within_json_string {
+                start_idx = Some(i);
+                break;
+            }
+        }
+
+        if start_idx.is_none() {
+            return None;
+        }
+
+        // satrt parsing from the opening brace
+        for (i, c) in content[start_idx.unwrap()..].chars().enumerate() {
+            if escape_char_next {
+                escape_char_next = false;
+                continue;
+            }
+
+            match c {
+                '\\' if within_json_string => escape_char_next = true ,
+                '"' => within_json_string = !within_json_string,
+                '{' if within_json_string => depth +=1,
+                '}' if within_json_string => {
+                    depth -= 1;
+                    if depth == 0 {
+                        let json_extract = &content[start_idx.unwrap()..=start_idx.unwrap() + i];
+
+                        if serde_json::from_str::<serde_json::Value>(json_extract).is_ok() {
+                            let remainder = content[start_idx.unwrap() + i + 1..].to_string();
+                            return Some((json_extract.to_string() , remainder));
+                        }
+                    }
+                },
+                _ => {}
+            }
+        }
+
+        None
+
     }
 }
 
@@ -40,26 +86,42 @@ impl LogSource for NetworkLogSource {
 
         if let Some(reader) = &mut self.reader {
 
-            loop {
-                let mut line = String::new();
-                let bytes_read = reader.read_line(&mut line).await?;
-
-                if bytes_read == 0 {
-                    return Ok(None);
+            // firstly check if the buffer already has something we need to check and extract
+            if !self.buffer.is_empty() {
+                if let Some((json, remainder)) = NetworkLogSource::try_extracting_json(&self.buffer) {
+                    self.buffer = remainder;
+                    return Ok(Some(LogLine { content:json, source: format!("network {}", self.address), timestamp: chrono::Utc::now() }));
                 }
-
-                self.buffer.push_str(&line);
-                if NetworkLogSource::is_valid_json(&self.buffer) {
-                    let content = std::mem::take(&mut self.buffer);
-                    return Ok(Some(LogLine {
-                        content: content.trim().to_string(),
-                        source: format!("Network {}", self.address),
-                        timestamp: chrono::Utc::now()
-                    }));
-                }
-
             }
 
+            let mut line = String::new();
+            let bytes_read = reader.read_line(&mut line).await?;
+
+            if bytes_read == 0 {
+                // here we need to check if the buffer now contains any json
+                if !self.buffer.is_empty() {
+                    let content = std::mem::take(&mut self.buffer);
+                    return Ok(Some(LogLine { content, source: format!("network {}", self.address), timestamp: chrono::Utc::now() }));
+                }
+                return Ok(None);
+            }
+
+            self.buffer.push_str(&line);
+            if let Some((json,remainder)) = NetworkLogSource::try_extracting_json(&self.buffer) {
+                // found a valid json , need to update the buffer
+                self.buffer = remainder;
+
+                return Ok(Some(LogLine { content: json, source: format!("network {}", self.address), timestamp: chrono::Utc::now() }));
+            }
+
+
+            // just a check here if the buffer content doesn't have any { or  } we can have it as a plain text
+            if !line.contains('{') && !line.contains('}') {
+                self.buffer.clear();
+                return Ok(Some(LogLine { content: line.trim().to_string(), source: format!("network {}", self.address), timestamp: chrono::Utc::now() }));
+            }
+
+            Ok(None)
         }
         else {
             Err("Source not initialised".into())
@@ -72,3 +134,8 @@ impl LogSource for NetworkLogSource {
         Ok(())
     }
 }
+
+
+
+// we have a plain text directly 
+// initially accumate in the buffer
